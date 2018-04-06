a0c174003e9fde8d3d2525a47ded3ed1445ca5ff
SkylerTyler1337 <tylernichols409@gmail.com>
2018-04-06 13:17:56 -0500

    Most Named Locations Added

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+
+class Main {
+  
+  static List<String> namedLocations = new ArrayList<String>();
+  
+  public void addNamedLocations(){
+    namedLocations.add("Retail Row");
+    namedLocations.add("Pleasant Park");
+    namedlocations.add("Tilted Towers");
+    namedLocations.add("Greasy Grove");
+    namedlocations.add("Dusty Depot");
+    namedLocation.add("Anarchy Acres");
+    namedlocations.add("Fatal Fields");
+    namedlocations.add("Lonely Lodge");
+    namedlocations.add("Tomato Town");
+    namedlocations,add("Loot Lake");
+    namedlocations.add("Shifty Shafts");
+    namedlocations.add("Flush Factory");
+    namedlocations.add("Snobby Shores");
+    namedlocations.add("Haunted Hills");
+    namedlocations.add("Junk Junction");
+    namedlocations.add("Lucky Landing");
+  }
